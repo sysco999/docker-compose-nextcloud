d92e739655d6065dbdae19345683981fa27007e2
sysco999 <158301388+sysco999@users.noreply.github.com>
2024-02-24 19:11:16 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+$CONFIG = array (
+  'dbtype' => 'pgsql',
+  'dbname' => 'nextcloud',
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'mysecretpassword',
+  'dbhost' => 'db',
+  'dbport' => '5432',
+  'dbtableprefix' => '',
+  'installed' => false,
+);
+
+
+
+
